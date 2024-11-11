d207fe4715becc5bf9761af45e5e65095736eb56
Malika Shrestha <shresthamalika71@gmail.com>
2024-11-10 21:38:18 -0600

    Update api_keys-checkpoint.py

--- .ipynb_checkpoints/api_keys-checkpoint.py
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "c38d8912701d36187bb9d366cabc8ae1"
+weather_api_key = "API Key"
 
 # Geoapify API Key
-geoapify_key = "7757f89767bc454db74be2b640389f77"
+geoapify_key = "API Key"
